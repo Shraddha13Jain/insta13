9cadb728b660132e879821e3eb64580fef94e3e3
Shraddha13Jain <shraddhaj829@gmail.com>
2021-06-07 12:49:39 +0530

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express();
 const bodyparser=require('body-parser');
 const mongoose = require('mongoose');
+const port=process.env.PORT || 5000;
 const cors=require('cors');
 //const PORT = 3000;
 mongoose.set('useFindAndModify', false);
@@ -38,14 +39,19 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 
-app.use('*',(req,res,next)=>{
-    res.status(404).json({"msg":"NOT FOUND"});
-});
+// app.use('*',(req,res,next)=>{
+//     res.status(404).json({"msg":"NOT FOUND"});
+// });
 
-const port=process.env.PORT || 5000;
 
-if(process.env.NODE_ENV==="production"){
+
+if(process.env.NODE_ENV=="production"){
     app.use(express.static("client/build"));
+    const path=require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+
 }
 app.listen(port,(err)=>{
     if(err) console.log(err);
